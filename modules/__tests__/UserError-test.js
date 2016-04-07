e35542df860598b1835cc137dce1edaee52317bd
Michael Jackson <mjijackson@gmail.com>
2016-04-07 18:01:59 +1000

    Remove nesting functionality

--- modules/__tests__/UserError-test.js
+++ modules/__tests__/UserError-test.js
@@ -2,110 +2,39 @@ import expect from 'expect'
 import UserError from '../index'
 
 describe('A UserError', () => {
-  describe('without a cause', () => {
-    let message, err
-    beforeEach(() => {
-      message = 'Bang!'
-      err = new UserError(message)
-    })
-
-    it('knows its name', () => {
-      expect(err.name).toEqual('UserError')
-    })
-
-    it('knows its message', () => {
-      expect(err.message).toEqual(message)
-    })
-
-    it('does not have a cause', () => {
-      expect(err.cause).toNotExist()
-    })
-
-    it('has a fullStack property', () => {
-      expect(typeof err.fullStack).toEqual('string')
-    })
+  let message, error
+  beforeEach(() => {
+    message = 'Bang!'
+    error = new UserError(message)
   })
 
-  describe('with a cause', () => {
-    let cause, message, err
-    beforeEach(() => {
-      cause = new UserError
-      message = 'Another bang!'
-      err = new UserError(message, cause)
-    })
+  it('knows its name', () => {
+    expect(error.name).toEqual('UserError')
+  })
 
-    it('knows its name', () => {
-      expect(err.name).toEqual('UserError')
-    })
-
-    it('knows its message', () => {
-      expect(err.message).toEqual(message)
-    })
-
-    it('has a cause', () => {
-      expect(err.cause).toExist()
-    })
-
-    it('has a fullStack property', () => {
-      expect(typeof err.fullStack).toEqual('string')
-    })
+  it('knows its message', () => {
+    expect(error.message).toEqual(message)
   })
 })
 
 describe('A UserError subclass instance', () => {
-  function MyError(message, cause) {
-    message = message || 'Boom!'
-    UserError.call(this, message, cause)
+  class MyError extends UserError {
+    constructor(message = 'Boom!') {
+      super(message)
+    }
   }
 
-  Object.setPrototypeOf(MyError.prototype, UserError.prototype)
-
-  describe('without a cause', () => {
-    let message, err
-    beforeEach(() => {
-      message = 'Boom!'
-      err = new MyError(message)
-    })
-
-    it('knows its name', () => {
-      expect(err.name).toEqual('MyError')
-    })
-
-    it('knows its message', () => {
-      expect(err.message).toEqual(message)
-    })
-
-    it('does not have a cause', () => {
-      expect(err.cause).toNotExist()
-    })
-
-    it('has a fullStack property', () => {
-      expect(typeof err.fullStack).toEqual('string')
-    })
+  let message, error
+  beforeEach(() => {
+    message = 'Boom!'
+    error = new MyError(message)
   })
 
-  describe('with a cause', () => {
-    let cause, message, err
-    beforeEach(() => {
-      cause = new UserError
-      message = 'Another boom!'
-      err = new MyError(message, cause)
-    })
+  it('knows its name', () => {
+    expect(error.name).toEqual('MyError')
+  })
 
-    it('knows its name', () => {
-      expect(err.name).toEqual('MyError')
-    })
-
-    it('knows its message', () => {
-      expect(err.message).toEqual(message)
-    })
-
-    it('has a cause', () => {
-      expect(err.cause).toExist()
-    })
-
-    it('has a fullStack property', () => {
-      expect(typeof err.fullStack).toEqual('string')
-    })
+  it('knows its message', () => {
+    expect(error.message).toEqual(message)
   })
 })
--- modules/index.js
+++ modules/index.js
@@ -1,30 +1,26 @@
-/**
- * An Error subclass that is better-suited for subclassing and
- * is nestable. Arguments are the error message and an optional
- * cause, which should be another error object that was responsible
- * for causing this error at some lower level.
- */
-function UserError(message, cause) {
+function UserError(message) {
   Error.call(this)
-  Error.captureStackTrace(this, this.constructor)
+
+  if (Error.captureStackTrace)
+    Error.captureStackTrace(this, this.constructor)
+
   this.name = this.constructor.name
   this.message = message
-  this.cause = cause
 }
 
-Object.setPrototypeOf(UserError.prototype, Error.prototype)
-
-Object.defineProperty(UserError.prototype, 'fullStack', {
-  configurable: true,
-  enumerable: true,
-  get() {
-    let stack = this.stack
-
-    if (this.cause)
-      stack += `\nCaused by ${this.cause.fullStack || this.cause.stack}`
-
-    return stack
+UserError.prototype = Object.create(Error.prototype, {
+  constructor: {
+    value: UserError,
+    configurable: true,
+    enumerable: false,
+    writable: true
   }
 })
+
+if (Object.setPrototypeOf) {
+  Object.setPrototypeOf(UserError, Error)
+} else {
+  UserError.__proto__ = Error
+}
 
 export default UserError
